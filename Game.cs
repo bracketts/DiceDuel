6f86c27bbc49e512c8454a26278a15ed85dc4faa
Guillermo Alvarado <116471742+bracketts@users.noreply.github.com>
2026-06-09 02:22:40 -0600

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,34 @@
+﻿Random dice = new();
+
+int playerScore = 0;
+int enemyScore = 0;
+
+while (playerScore <= 10 && enemyScore <= 10)
+{
+    int playerRoll = dice.Next(1, 7);
+    int enemyRoll = dice.Next(1, 7);
+
+    Console.WriteLine($"Player rolled a: {playerRoll}");
+    playerScore += playerRoll;
+    Console.WriteLine($"Enemy rolled a: {enemyRoll}");
+    enemyScore += enemyRoll;
+
+    if (playerScore > 10 || enemyScore > 10)
+    {
+        break;
+    }
+
+    Console.WriteLine($"Current Scores: Player: {playerScore} | Enemy: {enemyScore}");
+
+    string roundResult = playerRoll.CompareTo(enemyRoll) switch
+    {
+        > 0 => "Player wins this turn!",
+        < 0 => "Enemy wins this round!",
+        _ => "It's a tie this round!"
+    };
+
+    Console.WriteLine(roundResult);
+}
+Console.WriteLine($"Final Scores: Player: {playerScore} | Enemy: {enemyScore}");
+
+Console.WriteLine(playerScore > enemyScore ? "Player wins the fight!" : "Enemy wins the fight!");
